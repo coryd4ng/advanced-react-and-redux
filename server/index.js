b175ecf0f2909dde6093d26fa85c776a19fb42d1
coryd4ng <cory.q.dang@gmail.com>
2016-11-06 22:13:37 -0800

    64. Express Middleware

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ const morgan = require('morgan');
 const app = express();
 
 // App setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
 
 // Server setup
-const port = process.env.port || 3090;
+const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on port:', port);
